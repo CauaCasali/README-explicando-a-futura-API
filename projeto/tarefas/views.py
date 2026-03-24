4d6830cb44f25b4e07c6d1e103ea81fb1679eb9e
CauaCasali <casalicacau@gmail.com>
2026-03-23 21:52:23 -0300

    Adiciona app tarefas e campo prioridade ao modelo Tarefa

--- projeto/tarefas/views.py
+++ projeto/tarefas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Tarefa
+
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all().values()
+    return JsonResponse(list[any](tarefas), safe=False)
 
 # Create your views here.
